21bbf154011eb23957d2e7c9fd8ada420f4ad8cb
AndresRoaMercadoLibre <andres.roa@mercadolibre.com.co>
2022-11-21 13:00:39 -0500

    test pruebas pass

--- model/mock/hello.go
+++ model/mock/hello.go
@@ -3,5 +3,8 @@ package mock
 import "TDD/model"
 
 func Hello(name string) string {
+	if name == "" {
+		name = "World"
+	}
 	return model.ENGLISHELLOPREFIX + name
 }
--- model/mock/tests/hello_test.go
+++ model/mock/tests/hello_test.go
@@ -27,16 +27,19 @@ func TestHelloFailed(t *testing.T) {
 		got := mock.Hello("Chris")
 		want := "Hello, Chris"
 
-		if got != want {
-			t.Errorf("got  %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 	t.Run("Say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
 		got := mock.Hello("")
 		want := "Hello, World"
 
-		if got != want {
-			t.Errorf("got  %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got  %q want %q", got, want)
+	}
 }
